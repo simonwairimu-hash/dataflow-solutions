cdcfdf919f77f4b3bc5df453e076e553c9e17740
simonwairimu-hash <wairimusimon53@gmail.com>
2025-07-30 14:06:19 +0300

    final

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
+// next.config.js
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  
   typescript: {
+    
     // ignoreBuildErrors: true,
   },
 };
